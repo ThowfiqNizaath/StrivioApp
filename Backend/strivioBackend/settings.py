a7154cc0bdd3b257d95b4c2437707f0a42c9a130
ThowfiqNizaath <smartnizath@gmail.com>
2026-04-12 22:41:14 +0530

    Initial commit

--- Backend/strivioBackend/settings.py
+++ Backend/strivioBackend/settings.py
@@ -105,8 +105,13 @@ WSGI_APPLICATION = 'strivioBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.yziputnyexrvjxagcdyg',
+        'PASSWORD': 'Abcd23inas09',
+        'HOST': 'aws-1-ap-northeast-1.pooler.supabase.com',
+        'PORT': '6543',
+        'database': 'postgres'
     }
 }
 
